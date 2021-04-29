6f3f6249f1171bdefc857754d38d3bfb4d414c76
Rtrunzo <richtrunzo@gmail.com>
2021-04-29 15:51:58 -0700

    Added correct info to colorbox via props and updated colorbox styling.

--- src/color-box.js
+++ src/color-box.js
@@ -2,10 +2,17 @@ import React, {useState} from 'react';
 import "./colorbox.css";
 
 function ColorBox(props) {
+  console.log(props)
+  const {name, background} = props;
   return (
-    <div style={{ background: props.background }} className="ColorBox">
-      <span>MORE</span>
-      <span>{props.name}</span>
+    <div style={{ background }} className="ColorBox">
+      <div className="copy-container">
+      <div className="box-content">
+        <span>{name}</span>
+      </div>
+      <button className="copy-button">Copy</button>
+      </div>
+      <span className="see-more">More</span>
     </div>
   )
 }
