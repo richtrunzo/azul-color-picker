4361bb7abe3fa8222f70fc8445b9a6f0ef431113
Rtrunzo <richtrunzo@gmail.com>
2021-04-30 09:37:02 -0700

    Added copy overlay when a user copies a color to their clipboard.

--- src/color-box.js
+++ src/color-box.js
@@ -3,11 +3,25 @@ import "./colorbox.css";
 import {CopyToClipboard} from 'react-copy-to-clipboard';
 
 function ColorBox(props) {
-  console.log(props)
+  const [copy, setCopy] = useState(false);
+
+  const changeCopy = () => {
+    setCopy(true)
+    setTimeout(() =>  {
+    setCopy(false); }, 1500);
+  }
+
+
   const {name, background} = props;
+
   return (
-    <CopyToClipboard text={background}>
+    <CopyToClipboard text={background} onCopy={changeCopy}>
     <div style={{ background }} className="ColorBox">
+      <div style={{ background }} className={`copy-overlay ${copy && "show"}`}></div>
+      <div className={`copy-msg ${copy && "show"}`}>
+        <h1>copied!</h1>
+        <p>{background}</p>
+      </div>
       <div className="copy-container">
       <div className="box-content">
         <span>{name}</span>
