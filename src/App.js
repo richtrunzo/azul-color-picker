923ca3b9e4a596a76cf37d6ee42ceb2266d7c027
Rtrunzo <richtrunzo@gmail.com>
2021-05-01 17:02:22 -0700

    Added helper functions to generate dynamic color palette.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Palette from './pallette'
 import colors from './startColors'
+import generatePalette from './lib/color-helpers'
 
 function App() {
+  console.log(generatePalette(colors[4]))
   return (
     <div>
       <Palette {...colors[4]}/>
